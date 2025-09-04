b231e94b08d0b1e42a593af2c97babb8bf6572da
Javier Huertas <74338684+JaviRexUITU25@users.noreply.github.com>
2025-09-03 22:41:07 -0600

    Add files via upload

---
+++ PROYECTO 1(IMPORTANTE).py
@@ -0,0 +1,5 @@
+print("-"*10 + "¡BIENVENIDO A LA ELECCION DE NOMBRE!" + "-"* 10 + "\n"
+      "-"*9 + "Recuerde que su nombre debe ser creativo y ademas gracioso." + "-" * 9)
+first_name = input("Ingrese primer nombre: ")
+second_name = input("Ingrese segundo nombre: ")
+print(f"Tu nombre creativo es: ",first_name, second_name, end= "")
