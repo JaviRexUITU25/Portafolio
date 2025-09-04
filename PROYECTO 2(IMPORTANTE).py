5a70a851853646f172cf91e79967c552c75c0e37
Javier Huertas <74338684+JaviRexUITU25@users.noreply.github.com>
2025-09-03 22:40:50 -0600

    Add files via upload

---
+++ PROYECTO 2(IMPORTANTE).py
@@ -0,0 +1,7 @@
+name = input("Ingrese nombre: ")
+sales = float(input("Ingrese ventas: "))
+comition = sales * 13/100
+total = sales + comition
+print(f"El total de comisiones de tus ventas es de Q. {comition}\n"
+      f"El total es de: Q. {total}")
+
